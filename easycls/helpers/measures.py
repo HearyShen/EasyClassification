faf8ac43d0f9b623075c13768400e0c7bb530386
HearyShen <jiayun.shen@foxmail.com>
2020-02-15 23:29:25 +0800

    update measures

--- easycls/helpers/measures.py
+++ easycls/helpers/measures.py
@@ -1,7 +1,9 @@
 import torch
 
 def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
+    """
+    Computes the accuracy over the k top predictions for the specified values of k
+    """
     with torch.no_grad():
         maxk = max(topk)
         batch_size = target.size(0)
