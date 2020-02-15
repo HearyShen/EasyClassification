0fa2714c7c136119477641b18a0877277ce95816
HearyShen <jiayun.shen@foxmail.com>
2020-02-15 12:08:34 +0800

    implement logs.init_module_logger (code, docs)

--- easycls/helpers/logs.py
+++ easycls/helpers/logs.py
@@ -2,19 +2,43 @@ import logging
 
 
 def init_module_logger():
-    log = logging.getLogger(__name__)
-    log.addHandler(logging.NullHandler())
-    return log
+    """
+    Init a logger for the a module.
 
+    Usage:
 
-# Example function (for testing)
-def func():
-    log.critical('A Critical Error')
+    In module (demo.py):
 
+    ```python
+    from ..helpers import logs
+    logger = logs.init_module_logger()
 
-log = init_module_logger()
+    def func():
+        logger.debug('debug...')
+        logger.info('info...')
+        logger.warning('warning...')
+        logger.error('error...')
+        logger.critical('critical...')
+    ```
 
-if __name__ == "__main__":
-    print(__name__)
-    print(__package__)
-    print(__doc__)
+    In caller (test.py):
+    
+    ```python
+    import logging  # REMOVE this if you DON'T need logs
+    import easycls.apis.demo as demo
+
+    logging.basicConfig()   # REMOVE this if you DON'T need logs
+    demo.func()
+    ```
+
+    After execution, you should see the following outputs in `stdout`:
+    
+    ```
+    WARNING:easycls.helpers.logs:warning...
+    ERROR:easycls.helpers.logs:error...
+    CRITICAL:easycls.helpers.logs:critical...
+    ```
+    """
+    logger = logging.getLogger(__name__)
+    logger.addHandler(logging.NullHandler())
+    return logger
--- tools/dirtest.py
+++ tools/dirtest.py
@@ -1,10 +1,23 @@
-from os.path import dirname, abspath, join
+import os
+# insert root dir path to sys.path to import easycls
+import sys
+sys.path.insert(0,
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
-filename = abspath(__file__)
-print(f'current filepath: {filename}')
+# from os.path import dirname, abspath, join
 
-dirpath = abspath(dirname(filename))
-print(f'current dirpath: {dirpath}')
+# filename = abspath(__file__)
+# print(f'current filepath: {filename}')
 
-parent_dirpath = abspath(join(dirpath, '..'))
-print(f'parent dirpath: {parent_dirpath}')
+# dirpath = abspath(dirname(filename))
+# print(f'current dirpath: {dirpath}')
+
+# parent_dirpath = abspath(join(dirpath, '..'))
+# print(f'parent dirpath: {parent_dirpath}')
+
+import logging
+# import easycls.apis.infer as infer
+from easycls.apis.infer import func
+
+logging.basicConfig()
+func()
