e602b0dcbf793d92811e06ae2ba386b955fac06e
HearyShen <jiayun.shen@foxmail.com>
2020-02-19 00:06:38 +0800

    updates to docs and unit test

--- easycls/helpers/configs.py
+++ easycls/helpers/configs.py
@@ -1,16 +1,42 @@
 from configparser import ConfigParser
 
-# https://www.liaoxuefeng.com/wiki/1016959663602400/1018490750237280
-# config default and overide
 
 def parse_cfgs(path):
+    """
+    Parse a configuration file
+
+    Usage:
+
+    ```python
+    cfgs = parse_cfgs('config.ini')
+
+    task = cfgs.get("data", "task")
+    pretrain = cfgs.getboolean("model", "pretrained")
+    num_classes = cfgs.getint("data", "num-classes")
+    lr = cfgs.getfloat("learning", "learning-rate")
+    ```
+
+    Args:
+        path: configuration file path
+    
+    Return:
+        an instance of ConfigParser
+    """
     cfgparser = ConfigParser()
     cfgparser.read(path)
 
-    # sections = cfgparser.sections()
-    # # print(cfgparser._sections)
-    # for sect in sections:
-    #     print(cfgparser._sections[sect])
-
-    # return cfgparser._sections
     return cfgparser
+
+
+# Unit Test
+if __name__ == "__main__":
+    cfgs = parse_cfgs('config.ini')
+
+    task = cfgs.get("data", "task")
+    pretrain = cfgs.getboolean("model", "pretrained")
+    num_classes = cfgs.getint("data", "num-classes")
+    lr = cfgs.getfloat("learning", "learning-rate")
+
+    print(
+        f'task={task}\tpretrain={pretrain}\tnum-classes-{num_classes}\tlr={lr}'
+    )
