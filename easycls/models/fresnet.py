9950a95be1da48f8f6df3adebcac30d8e75bd9ab
HearyShen <jiayun.shen@foxmail.com>
2020-03-30 22:59:43 +0800

    fix a bug in fresnet.py

--- easycls/models/fresnet.py
+++ easycls/models/fresnet.py
@@ -5,81 +5,96 @@ import torch
 import torchvision.models.resnet as resnet
 
 
-def fresnet18(pretrained=False, progress=True, num_classes=1000, **kwargs):
-    r"""AutoFit ResNet-18 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+def fresnet18(pretrained=False, num_classes=1000, **kwargs):
+    r"""AutoFit ResNet-18 model.
+    Fit the pretrained model for target num_classes by replacing the last fc layer.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
-        num_classes (int): If pretrained is ture and num_classes is not default 1000, model's tail will be replaced after loading pretrained model.
+        num_classes (int): If pretrained is ture and num_classes is not default 1000, 
+                        model's last fc layer will be replaced after loading pretrained model.
     """
-    model = resnet.resnet18(pretrained, progress)
     if pretrained and num_classes != 1000:
+        model = resnet.resnet18(pretrained, **kwargs)
         model.fc = torch.nn.Linear(512 * resnet.BasicBlock.expansion,
                                    num_classes)
+    else:
+        model = resnet.resnet18(pretrained, num_classes=num_classes, **kwargs)
     return model
 
 
-def fresnet34(pretrained=False, progress=True, num_classes=1000, **kwargs):
-    r"""AutoFit ResNet-34 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+def fresnet34(pretrained=False, num_classes=1000, **kwargs):
+    r"""AutoFit ResNet-34 model.
+    Fit the pretrained model for target num_classes by replacing the last fc layer.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
-        num_classes (int): If pretrained is ture and num_classes is not default 1000, model's tail will be replaced after loading pretrained model.
+        num_classes (int): If pretrained is ture and num_classes is not default 1000, 
+                        model's last fc layer will be replaced after loading pretrained model.
     """
-    model = resnet.resnet34(pretrained, progress)
     if pretrained and num_classes != 1000:
+        model = resnet.resnet34(pretrained, **kwargs)
         model.fc = torch.nn.Linear(512 * resnet.BasicBlock.expansion,
                                    num_classes)
+    else:
+        model = resnet.resnet34(pretrained, num_classes=num_classes, **kwargs)
     return model
 
 
-def fresnet50(pretrained=False, progress=True, num_classes=1000, **kwargs):
-    r"""AutoFit ResNet-50 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+def fresnet50(pretrained=False, num_classes=1000, **kwargs):
+    r"""AutoFit ResNet-50 model.
+    Fit the pretrained model for target num_classes by replacing the last fc layer.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
-        num_classes (int): If pretrained is ture and num_classes is not default 1000, model's tail will be replaced after loading pretrained model.
+        num_classes (int): If pretrained is ture and num_classes is not default 1000, 
+                        model's last fc layer will be replaced after loading pretrained model.
     """
-    model = resnet.resnet50(pretrained, progress)
     if pretrained and num_classes != 1000:
+        model = resnet.resnet50(pretrained, **kwargs)
         model.fc = torch.nn.Linear(512 * resnet.Bottleneck.expansion,
                                    num_classes)
+    else:
+        model = resnet.resnet50(pretrained, num_classes=num_classes, **kwargs)
     return model
 
 
-def fresnet101(pretrained=False, progress=True, num_classes=1000, **kwargs):
-    r"""AutoFit ResNet-101 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+def fresnet101(pretrained=False, num_classes=1000, **kwargs):
+    r"""AutoFit ResNet-101 model.
+    Fit the pretrained model for target num_classes by replacing the last fc layer.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
-        num_classes (int): If pretrained is ture and num_classes is not default 1000, model's tail will be replaced after loading pretrained model.
+        num_classes (int): If pretrained is ture and num_classes is not default 1000, 
+                        model's last fc layer will be replaced after loading pretrained model.
     """
-    model = resnet.resnet101(pretrained, progress)
     if pretrained and num_classes != 1000:
+        model = resnet.resnet101(pretrained, **kwargs)
         model.fc = torch.nn.Linear(512 * resnet.Bottleneck.expansion,
                                    num_classes)
+    else:
+        model = resnet.resnet101(pretrained, num_classes=num_classes, **kwargs)
     return model
 
 
-def fresnet152(pretrained=False, progress=True, num_classes=1000, **kwargs):
-    r"""AutoFit ResNet-152 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+def fresnet152(pretrained=False, num_classes=1000, **kwargs):
+    r"""AutoFit ResNet-152 model.
+    Fit the pretrained model for target num_classes by replacing the last fc layer.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
-        num_classes (int): If pretrained is ture and num_classes is not default 1000, model's tail will be replaced after loading pretrained model.
+        num_classes (int): If pretrained is ture and num_classes is not default 1000, 
+                        model's last fc layer will be replaced after loading pretrained model.
     """
-    model = resnet.resnet152(pretrained, progress)
     if pretrained and num_classes != 1000:
+        model = resnet.resnet152(pretrained, **kwargs)
         model.fc = torch.nn.Linear(512 * resnet.Bottleneck.expansion,
                                    num_classes)
+    else:
+        model = resnet.resnet152(pretrained, num_classes=num_classes, **kwargs)
     return model
